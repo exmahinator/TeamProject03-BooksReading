d9caf55b70ec0a1e3ae3264a1995b25a06a614c5
Vita Koval <mayevit@hotmail.com>
2022-10-19 17:02:54 +0300

    fix/position

--- src/ui/BasicStyles/Container.styled.js
+++ src/ui/BasicStyles/Container.styled.js
@@ -1,16 +1,18 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
+  position: relative;
+  z-index: 1;
   margin: 0 auto;
   padding: 0 20px;
 
   @media screen and (min-width: 768px) {
-      padding: 0 32px;
-    }
+    padding: 0 32px;
+  }
 
-@media screen and (min-width: 1280px) {
-      padding: 0 16px;
-    }
+  @media screen and (min-width: 1280px) {
+    padding: 0 16px;
+  }
 `;
 
 export default Container;
