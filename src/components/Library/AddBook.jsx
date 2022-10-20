2c8d81e77f48791e3dcd2698d0aca0a4727243ec
Yaroslav Drohoziuk <exmahinator@ukr.net>
2022-10-20 17:01:12 +0300

    Modified Library Add

--- src/components/Library/AddBook.jsx
+++ src/components/Library/AddBook.jsx
@@ -1,119 +1,129 @@
 import React from 'react';
 import { useState } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { AddBookToRead } from 'redux/library/libraryOperation';
 import {
-  LibraryForm,
-  LibrarySubContainer,
-  LibraryInput,
-  LibraryInputContainer,
-  LibraryNameContainer,
-  LibraryButton,
+	LibraryForm,
+	LibrarySubContainer,
+	LibraryInput,
+	LibraryInputContainer,
+	LibraryNameContainer,
+	LibraryButton,
 } from 'ui/LibraryPage';
 
 function AddBook() {
-  const [bookName, setBookName] = useState('');
-  const [bookAuthor, setBookAuthor] = useState('');
-  const [bookYear, setBookYear] = useState('');
-  const [bookPages, setBookPages] = useState('');
-  const [book, setBook] = useState('');
+	const [book, setBook] = useState('');
+	console.log('book:', book);
 
-  const onInput = event => {
-    const { name, value } = event.target;
-    switch (name) {
-      case 'name':
-        setBookName(value);
-        break;
-      case 'author':
-        setBookAuthor(value);
-        break;
-      case 'year':
-        setBookYear(value);
-        break;
-      case 'pages':
-        setBookPages(value);
-        break;
-      default:
-        alert('Something went wrong...');
-        return;
-    }
-  };
+	// const dispatch = useDispatch();
 
-  const handleAddBook = event => {
-    event.preventDefault();
-    console.log('clicking');
-    setBook({
-      name: bookName,
-      author: bookAuthor,
-      year: bookYear,
-      pages: bookPages,
-    });
-    console.log(book);
-  };
+	//   {
+	//   "title": "The Book of Five Rings",
+	//   "author": "Miyamoto Musashi",
+	//   "publishYear": 1643,
+	//   "totalPages": 110
+	// }
 
-  return (
-    <>
-      <LibraryForm onSubmit={handleAddBook}>
-        <LibraryNameContainer>
-          <label htmlFor="name">Назва книги</label>
-          <LibraryInput
-            id="name"
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Book name may contain only letters, apostrophe, dash and spaces"
-            value={bookName}
-            onChange={onInput}
-            placeholder="..."
-            required
-          />
-        </LibraryNameContainer>
-        <LibraryInputContainer>
-          <LibrarySubContainer>
-            <label htmlFor="author">Автор книги</label>
-            <LibraryInput
-              id="author"
-              type="text"
-              name="author"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              value={bookAuthor}
-              onChange={onInput}
-              placeholder="..."
-              required
-            />
-          </LibrarySubContainer>
-          <LibrarySubContainer>
-            <label htmlFor="year">Рік випуску</label>
-            <LibraryInput
-              id="year"
-              type="number"
-              name="year"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Year must be digits"
-              value={bookYear}
-              onChange={onInput}
-              placeholder="..."
-              required
-            />
-          </LibrarySubContainer>
-          <LibrarySubContainer>
-            <label htmlFor="pages">Кількість сторінок</label>
-            <LibraryInput
-              id="pages"
-              type="number"
-              name="pages"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Page number must be digits"
-              value={bookPages}
-              onChange={onInput}
-              placeholder="..."
-              required
-            />
-          </LibrarySubContainer>
-        </LibraryInputContainer>
-        <LibraryButton type="submit">Додати</LibraryButton>
-      </LibraryForm>
-    </>
-  );
+	// const onInput = event => {
+	// 	const { name, value } = event.target;
+	// 	switch (name) {
+	// 		case 'name':
+	// 			setBookName(value);
+	// 			break;
+	// 		case 'author':
+	// 			setBookAuthor(value);
+	// 			break;
+	// 		case 'year':
+	// 			setBookYear(value);
+	// 			break;
+	// 		case 'pages':
+	// 			setBookPages(value);
+	// 			break;
+	// 		default:
+	// 			alert('Something went wrong...');
+	// 			return;
+	// 	}
+	// };
+
+	const handleAddBook = event => {
+		event.preventDefault();
+		const form = event.currentTarget;
+		setBook({
+			title: form.elements.name.value,
+			author: form.elements.author.value,
+			publishYear: form.elements.year.value,
+			totalPages: form.elements.pages.value,
+		});
+
+		// dispatch(AddBookToRead({ title: bookName, author: bookAuthor, publishYear: bookYear, pagesTotal: bookPages }));
+		form.reset();
+	};
+
+	return (
+		<>
+			<LibraryForm onSubmit={handleAddBook}>
+				<LibraryNameContainer>
+					<label htmlFor="name">Назва книги</label>
+					<LibraryInput
+						id="name"
+						type="text"
+						name="name"
+						pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+						title="Book name may contain only letters, apostrophe, dash and spaces"
+						// value={bookName}
+						// onChange={onInput}
+						placeholder="..."
+						required
+					/>
+				</LibraryNameContainer>
+				<LibraryInputContainer>
+					<LibrarySubContainer>
+						<label htmlFor="author">Автор книги</label>
+						<LibraryInput
+							id="author"
+							type="text"
+							name="author"
+							pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+							title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+							// value={bookAuthor}
+							// onChange={onInput}
+							placeholder="..."
+							required
+						/>
+					</LibrarySubContainer>
+					<LibrarySubContainer>
+						<label htmlFor="year">Рік випуску</label>
+						<LibraryInput
+							id="year"
+							type="number"
+							name="year"
+							pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+							title="Year must be digits"
+							// value={bookYear}
+							// onChange={onInput}
+							placeholder="..."
+							required
+						/>
+					</LibrarySubContainer>
+					<LibrarySubContainer>
+						<label htmlFor="pages">Кількість сторінок</label>
+						<LibraryInput
+							id="pages"
+							type="number"
+							name="pages"
+							pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+							title="Page number must be digits"
+							// value={bookPages}
+							// onChange={onInput}
+							placeholder="..."
+							required
+						/>
+					</LibrarySubContainer>
+				</LibraryInputContainer>
+				<LibraryButton type="submit">Додати</LibraryButton>
+			</LibraryForm>
+		</>
+	);
 }
 
 export default AddBook;
--- src/redux/library/librarySlice.js
+++ src/redux/library/librarySlice.js
@@ -1,14 +1,69 @@
-import { createSlice } from '@reduxjs/toolkit';
+// import { createSlice } from '@reduxjs/toolkit';
+// import { addBookToRead } from './libraryOperation';
+// import { initialState } from 'config';
 
-const initialState = {
-    books: [],
-    statistics: [],
-};
+// export const initialState = {
+// 	accessToken: null,
+// 	refreshToken: null,
+// 	sid: null,
+// 	userData: {
+// 		name: null,
+// 		email: '',
+// 		goingToRead: [],
+// 		currentlyReading: [],
+// 		finishedReading: [],
+// 		id: null,
+// 	},
+// 	isLoggedIn: false,
+// 	isRefreshing: false,
+// 	error: null,
+// };
 
-export const librarySlice = createSlice({
-  name: 'library',
-  initialState,
-  extraReducers: {},
-});
+// {
+//   "title": "The Book of Five Rings",
+//   "author": "Miyamoto Musashi",
+//   "publishYear": 1643,
+//   "totalPages": 110,
+//   "pagesFinished": 0,
+//   "_id": "507f1f77bcf86cd799439013",
+//   "__v": 0
+// }
 
-export default librarySlice.reducer;
+// const librarySlice = createSlice({
+// 	name: 'book',
+// 	initialState,
+// 	extraReducers: {
+// 		[addBookToRead.fulfilled](state, action) {
+// 			return state.userData.goingToRead.push(action.payload);
+// 		},
+// [logIn.fulfilled](state, action) {
+// 	state.userData = action.payload.userData;
+// 	state.sid = action.payload.sid;
+// 	state.accessToken = action.payload.accessToken;
+// 	state.refreshToken = action.payload.refreshToken;
+// 	state.isLoggedIn = true;
+// },
+// [logOut.fulfilled](state) {
+// 	state.sid = null;
+// 	state.accessToken = null;
+// 	state.refreshToken = null;
+// 	state.isLoggedIn = false;
+// 	state.userData.email = '';
+// },
+// [refreshUser.pending](state) {
+// 	state.isRefreshing = true;
+// },
+// [refreshUser.fulfilled](state, action) {
+// 	state.accessToken = action.payload.newAccessToken;
+// 	state.refreshToken = action.payload.newRefreshToken;
+// 	state.sid = action.payload.newSid;
+// 	state.isLoggedIn = true;
+// 	state.isRefreshing = false;
+// },
+// [refreshUser.rejected](state) {
+// 	state.isRefreshing = false;
+// },
+// 	},
+// });
+
+// export const libraryReducer = librarySlice.reducer;
