be7dac4c4ec47f761f0764fc9ae49d25347088a2
EgorRybin <antoniopareiro@gmail.com>
2022-10-20 10:08:32 +0300

    update auth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,44 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { logOut, refreshUser } from 'redux/auth/authOperation';
+import { getUserEmail, getSid } from 'redux/auth/authSelector';
+import { getAccessToken } from 'redux/auth/authSelector';
+
 const Header = () => {
-    return (
-        <header style={{display: 'flex'}}>
-            <div>BR</div>
-            <div>
-                <span>M</span>
-                <p>Martha Stewart</p>
-            </div>
-            <div>
-                <span>a</span>
-                <span>s</span>
-                <span>d</span>
-            </div>
-        </header>
-    )
+	const dispatch = useDispatch();
+	const userEmail = useSelector(getUserEmail);
+	const sid = useSelector(getSid);
+	const accessToken = useSelector(getAccessToken);
+
+	return (
+		<header style={{ display: 'flex' }}>
+			<div>BR</div>
+			<div>
+				<span>M</span>
+				<p>{userEmail}</p>
+			</div>
+			<div>
+				<span>a</span>
+				<span>s</span>
+				<button
+					type="button"
+					onClick={() => {
+						dispatch(logOut(accessToken));
+					}}
+				>
+					Logout
+				</button>
+				<button
+					type="button"
+					onClick={() => {
+						dispatch(refreshUser({ sid }));
+					}}
+				>
+					Refresh
+				</button>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
--- src/redux/auth/authOperation.js
+++ src/redux/auth/authOperation.js
@@ -4,90 +4,80 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { setAuthHeader, clearAuthHeader } from 'config';
 import { useDispatch } from 'react-redux';
 
-// "password": "TestUser123"
-
-// Відповідь від бекенда при Register
-// {
-//   "name": "TestName",
-//   "email": "TestUser@example.com",
-//   "id": "635009c53551fd60da50fc5f"
-// }
-
-// Відповідь від бекенда при LogIn
-// {
-//   "accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2MzUwMDljNTM1NTFmZDYwZGE1MGZjNWYiLCJzaWQiOiI2MzUwMGEwMTM1NTFmZDYwZGE1MGZjNjAiLCJpYXQiOjE2NjYxODk4MjUsImV4cCI6MTY2NjE5MzQyNX0.0y-pfrTi-Sb30n8kn4pbea_Slpr0htUQpwIuvc24jis",
-//   "refreshToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2MzUwMDljNTM1NTFmZDYwZGE1MGZjNWYiLCJzaWQiOiI2MzUwMGEwMTM1NTFmZDYwZGE1MGZjNjAiLCJpYXQiOjE2NjYxODk4MjUsImV4cCI6MTY2ODgxNzgyNX0.Rxhd0ACkDihjDMIa5tqVlHzcse5d9BIABp0rye5z41M",
-//   "sid": "63500a013551fd60da50fc60",
-//   "userData": {
-//     "name": "TestName",
-//     "email": "TestUser@example.com",
-//     "goingToRead": [],
-//     "currentlyReading": [],
-//     "finishedReading": [],
-//     "id": "635009c53551fd60da50fc5f"
-//   }
-// }
-
 export const logIn = createAsyncThunk(
-  'auth/login',
-  async (credentials, thunkAPI) => {
-    try {
-      const res = await axios.post('/auth/login', credentials);
-      setAuthHeader(res.data.refreshToken);
-      return res.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
-    }
-  }
+	'auth/login',
+	async (credentials, thunkAPI) => {
+		try {
+			const { data } = await axios.post('/auth/login', credentials);
+			setAuthHeader(data.accessToken);
+			return data;
+		} catch (error) {
+			return thunkAPI.rejectWithValue(error.message);
+		}
+	}
 );
 
 export const register = createAsyncThunk(
-  'auth/register',
-  async (credentials, thunkAPI) => {
-    try {
-      const password = credentials.password;
-      const res = await axios.post('/auth/register', credentials);
-      const email = res.email;
-      const dispatch = useDispatch();
-      const loggedUser = dispatch(logIn({ email, password }));
+	'auth/register',
+	async (credentials, thunkAPI) => {
+		try {
+			const password = credentials.password;
+			const { data } = await axios.post('/auth/register', credentials);
+			const email = data.email;
+			const dispatch = useDispatch();
+			const loggedUser = dispatch(logIn({ email, password }));
 
-      setAuthHeader(loggedUser.data.refreshToken);
-      return logIn.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
-    }
-  }
+			setAuthHeader(loggedUser.data.refreshToken);
+			return logIn.data;
+		} catch (error) {
+			return thunkAPI.rejectWithValue(error.message);
+		}
+	}
 );
 
-// ------------------------------------------------------------------------------------------------------------------------
-// Доробити:
-// ------------------------------------------------------------------------------------------------------------------------
-
-export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
-  try {
-    await axios.post('/auth/logout');
-    clearAuthHeader();
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.message);
-  }
-});
+export const logOut = createAsyncThunk(
+	'auth/logout',
+	async (accessToken, thunkAPI) => {
+		setAuthHeader(accessToken);
+		try {
+			await axios.post('/auth/logout');
+			clearAuthHeader();
+		} catch (error) {
+			return thunkAPI.rejectWithValue(error.message);
+		}
+	}
+);
 
 export const refreshUser = createAsyncThunk(
-  'auth/refresh',
-  async (_, thunkAPI) => {
-    const state = thunkAPI.getState();
-    const persistedToken = state.auth.token;
+	'auth/refresh',
+	async (sid, thunkAPI) => {
+		const state = thunkAPI.getState();
+		const persistedToken = state.auth.refreshToken;
 
-    if (persistedToken === null) {
-      return thunkAPI.rejectWithValue('Unable to fetch user');
-    }
+		if (persistedToken === null) {
+			return thunkAPI.rejectWithValue('Unable to fetch user');
+		}
 
-    try {
-      setAuthHeader(persistedToken);
-      const res = await axios.get('/auth/refresh');
-      return res.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
-    }
-  }
+		try {
+			setAuthHeader(persistedToken);
+			const { data } = await axios.post('/auth/refresh', sid);
+			console.log(data);
+			return data;
+		} catch (error) {
+			return thunkAPI.rejectWithValue(error.message);
+		}
+	}
+);
+
+export const loginWithGoogle = createAsyncThunk(
+	'auth/loginwithgoogle',
+	async (_, thunkAPI) => {
+		// axios.defaults.headers.common['Access-Control-Allow-Origin'] = `*`;
+		try {
+			const res = await axios.get('/auth/google');
+			console.log(res);
+		} catch (error) {
+			return thunkAPI.rejectWithValue(error.message);
+		}
+	}
 );
