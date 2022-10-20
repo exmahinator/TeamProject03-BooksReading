f38ba6b1a85fc230b2f3f31ce2da9e00472134f0
Vita Koval <mayevit@hotmail.com>
2022-10-20 11:27:29 +0300

    add/routes private publik

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,63 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Layout } from "./Layout";
-import { AuthLogin } from "../pages/AuthLogi";
-import { LibraryPage } from "../pages/LibraryPage";
-import { TrainingPage } from "../pages/TrainingPage";
-import { StatisticsPage } from "../pages/StatisticsPage";
+import { Navigate } from 'react-router-dom';
+import { Layout } from './Layout';
+import { AuthLogin } from '../pages/AuthLogi';
+import { LibraryPage } from '../pages/LibraryPage';
+import { TrainingPage } from '../pages/TrainingPage';
+import { StatisticsPage } from '../pages/StatisticsPage';
 import { AuthRegistration } from '../pages/AuthRegistration';
+import PrivateRoute from './Route/PrivatePoute';
+import PublicRoute from './Route/PublicRoute';
 
 export const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/login" element={<AuthLogin />} />
-        <Route path="/register" element={<AuthRegistration />} />
-        <Route path="/library" element={<LibraryPage />} />
-        <Route path="/training" element={<TrainingPage />} />
-        <Route path="/statistics" element={<StatisticsPage />} />
+	return (
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				<Route index element={<Navigate to="/login" />} />
+				<Route
+					path="/login"
+					element={
+						<PublicRoute redirect="/library" restricted>
+							<AuthLogin />
+						</PublicRoute>
+					}
+				/>
+				<Route
+					path="/register"
+					element={
+						<PublicRoute redirect="/library" restricted>
+							<AuthRegistration />
+						</PublicRoute>
+					}
+				/>
+				<Route
+					path="/library"
+					element={
+						<PrivateRoute redirect="/">
+							<LibraryPage />
+						</PrivateRoute>
+					}
+				/>
+				<Route
+					path="/training"
+					element={
+						<PrivateRoute redirect="/">
+							<TrainingPage />
+						</PrivateRoute>
+					}
+				/>
+				<Route
+					path="/statistics"
+					element={
+						<PrivateRoute redirect="/">
+							<StatisticsPage />
+						</PrivateRoute>
+					}
+				/>
 
-        <Route path="*" element={<AuthLogin />} />
-      </Route>
-    </Routes>
-  );
+				<Route path="*" element={<AuthLogin />} />
+			</Route>
+		</Routes>
+	);
 };
---
+++ src/components/Route/PrivatePoute.js
@@ -0,0 +1,12 @@
+import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getIsLoggedIn } from '../../redux/selectors';
+
+
+function PrivateRoute({ children, redirect = '/' }) {
+  const isLoggedIn = useSelector(getIsLoggedIn);
+
+  return isLoggedIn ? children : <Navigate to={redirect} />;
+}
+
+export default PrivateRoute;
