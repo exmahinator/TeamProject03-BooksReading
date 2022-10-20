92e24f8f2c7f90efaeb0b9fb2ea4eac53e62b2fe
LeonidLukin <leonidlukin90@gmail.com>
2022-10-20 11:43:42 +0300

    resolved conflicts

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import { Container, Section } from 'ui/BasicStyles';
+import TrainingList from './TrainingList';
 
 import Header from './Header/Header';
 
@@ -7,7 +8,8 @@ import Header from './Header/Header';
 export const Layout = () => {
   return (
     <>
-     <Header />
+      HEADER
+      <TrainingList/>
       <Section>
         <Container>
           <Outlet />
