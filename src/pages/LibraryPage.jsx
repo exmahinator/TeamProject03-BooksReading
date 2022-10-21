e723e7c4a6743f16e857e69895639f499d00934f
Yaroslav Drohoziuk <exmahinator@ukr.net>
2022-10-21 10:04:30 +0300

    Add temp page change

--- src/pages/LibraryPage.jsx
+++ src/pages/LibraryPage.jsx
@@ -1,6 +1,7 @@
 import { AddBook } from 'components/Library';
 import { Section, Container } from 'ui/BasicStyles';
 import { LibraryContainer } from 'ui/LibraryPage';
+import { NavLink } from 'react-router-dom';
 
 export const LibraryPage = () => {
 	return (
@@ -8,6 +9,7 @@ export const LibraryPage = () => {
 			<Container>
 				<LibraryContainer>
 					<AddBook />
+					<NavLink to="/training">training</NavLink>
 				</LibraryContainer>
 			</Container>
 		</Section>
