82cd65ae98309e2615701170dbce0ec1da8e3dfb
Vita Koval <mayevit@hotmail.com>
2022-10-22 15:40:02 +0300

    fix/dataCalendar

--- src/components/MyTraining/MyTraining.jsx
+++ src/components/MyTraining/MyTraining.jsx
@@ -7,19 +7,29 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 // import {getIsLoggedIn} from '../../redux/selectors';
 import { addBookPlanning } from '../../redux/library/libraryOperation';
+import { getGoingToRead } from "../../redux/library/librarySelector";
 import { Wrapper, Title, BoxForm, Button } from './MyTraining.styled';
 
-export default function MyTraining() {
+// "startDate": "2022-10-20",
+	//   "endDate": "2022-10-25",
+	//   "books": [
+	//     "635150dd3551fd60da50fed6", "507f1f77bcf86cd799439013"
+	//   ]
 
+
+export default function MyTraining() {
+	const dispatch = useDispatch();
 	const [personName, setPersonName] = useState([]);
 	const [start, setStart] = useState(null);
-	const [finish, setFinish] = useState(null);
+  const [finish, setFinish] = useState(null);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [books, setBooks] = useState(null);
 
 	// const books = useSelector(getIsLoggedIn);
-	const dispatch = useDispatch();
 
 	const ITEM_HEIGHT = 48;
 	const ITEM_PADDING_TOP = 8;
@@ -32,60 +42,57 @@ export default function MyTraining() {
 		},
 	};
 
-	const names = [
-		'Oliver Hansen',
-		'Van Henry',
-		'April Tucker',
-		'Ralph Hubbard',
-		'Omar Alexander',
-		'Carlos Abbott',
-		'Miriam Wagner',
-		'Bradley Wilkerson',
-		'Virginia Andrews',
-		'Kelly Snyder',
-	];
+	// const names = [
+	// 	'Oliver Hansen',
+	// 	'Van Henry',
+	// 	'April Tucker',
+	// 	'Ralph Hubbard',
+	// 	'Omar Alexander',
+	// 	'Carlos Abbott',
+	// 	'Miriam Wagner',
+	// 	'Bradley Wilkerson',
+	// 	'Virginia Andrews',
+	// 	'Kelly Snyder',
+  // ];
+  
+  const goingToRead = useSelector(getGoingToRead);
 
 	// Дата старту - готова до використання---
 	const receiveDataFromStart = newValue => {
-		console.log(newValue);
-		console.log(newValue.$y);
-		console.log(newValue.$M + 1);
-		console.log(newValue.$D + 1);
-		const startDate = `${newValue.$y}-${newValue.$M + 1}-${newValue.$D + 1}`;
-		console.log(startDate);
-	};
+    const startDate = `${newValue.$y}-${newValue.$M + 1}-${newValue.$D + 1}`;
+    setStartDate(startDate);
+		// console.log('startDate', startDate);
+  };
+  
+  const receiveDataFromEnd = newValue => {
+    const endDate = `${newValue.$y}-${newValue.$M + 1}-${newValue.$D + 1}`;
+    setEndDate(endDate);
+  }
 	// ----------------------------------------
+  // console.log('startDate', startDate);
+  // console.log('endDate', endDate);
+  // ----------------------------------------
 
-	const handleChange = event => {
-		// console.log(event)
+  const handleChange = event => {
+    console.log(event)
 		const {
-			target: { value },
+      target: { value },
 		} = event;
-		setPersonName(typeof value === 'string' ? value.split(',') : value);
-	};
+    setPersonName(typeof value === 'string' ? value.split(',') : value);
 
-	// const startTime = {startDate: `${start['$y']} - ${start['$M']+1} - ${start['$D']}`};
-	// const finishTime = {endDate: `${finish['$y']} - ${finish['$M']+1} - ${finish['$D']}`};
-	// const goingToBooks = books.id;
-
-	// console.log('startTime:', startTime)
-	// console.log('finishTime:', finishTime)
-	// "startDate": "2022-10-20",
-	//   "endDate": "2022-10-25",
-	//   "books": [
-	//     "635150dd3551fd60da50fed6", "507f1f77bcf86cd799439013"
-	//   ]
+  };
+    //  console.log("personName", personName)
 
 	const handleSubmit = event => {
 		event.preventDefault();
-		dispatch(addBookPlanning({ start, finish, personName }));
+		dispatch(addBookPlanning({ startDate, endDate, personName }));
 		setStart(null);
 		setFinish(null);
 		setPersonName([]);
 
 		console.log(handleSubmit);
 	};
-	// console.log(start)
+
 
 	return (
 		<Wrapper>
@@ -96,19 +103,6 @@ export default function MyTraining() {
 						label="Початок"
 						value={start}
 						disablePast={true}
-						// onChange={newValue => {
-						// 	console.log(newValue);
-						// 	setStart(newValue);
-						// 	console.log(Object.values(newValue));
-						// 	const someStart = Object.values(newValue);
-						// 	console.log(someStart);
-						// 	const resultData = {
-						// 		data1: someStart[4],
-						// 		data2: someStart[5] + 1,
-						// 		data3: someStart[6],
-						// 	};
-						// 	console.log(resultData);
-						// }}
 						onChange={receiveDataFromStart}
 						renderInput={params => <TextField {...params} />}
 					/>
@@ -119,9 +113,7 @@ export default function MyTraining() {
 						label="Завершення"
 						value={finish}
 						disablePast={true}
-						onChange={newValue => {
-							setFinish(newValue);
-						}}
+            onChange={receiveDataFromEnd}
 						renderInput={params => <TextField {...params} />}
 					/>
 				</LocalizationProvider>
@@ -146,9 +138,9 @@ export default function MyTraining() {
 						<MenuItem disabled value="">
 							<em>Placeholder</em>
 						</MenuItem>
-						{names.map(name => (
-							<MenuItem key={name} value={name}>
-								{name}
+						{goingToRead?.map(({_id, title, author}) => (
+							<MenuItem key={_id} value={title} name={_id}>
+								{title} ({author})
 							</MenuItem>
 						))}
 					</Select>
