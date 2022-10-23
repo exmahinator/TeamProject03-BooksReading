47f52d77c9600abc0c9d1059d6bf98b4c5c5186d
Demchenk0 <santadyr2016@gmail.com>
2022-10-23 14:44:50 +0300

    styledTel

---
+++ src/ui/StatisticsPage/StatSubContainer.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const StatSubContainer = styled.div`
+	width: 110px;
+	display: flex;
+	flex-direction: column;
+`;
+
+export default StatSubContainer;
--- src/ui/StatisticsPage/StatisticsButton.styled.js
+++ src/ui/StatisticsPage/StatisticsButton.styled.js
@@ -5,6 +5,18 @@ const StatisticsButton = styled.button`
 	color: white;
 	border: none;
 	padding: 10px 45px;
+	font-family: 'Montserrat';
+	font-style: normal;
+	font-weight: 600;
+	font-size: 16px;
+	line-height: 20px;
+	/* identical to box height */
+
+	text-align: center;
+
+	color: #ffffff;
+	margin-top: 28px;
+	margin-bottom: 24px;
 `;
 
 export default StatisticsButton;
