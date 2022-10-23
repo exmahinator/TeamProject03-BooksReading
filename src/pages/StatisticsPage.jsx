a726711edf599383c31dcf8c5d5072e33f9e405d
Vita Koval <mayevit@hotmail.com>
2022-10-23 18:25:19 +0300

    fix/stsatisticsPage and TrainingList

--- src/pages/StatisticsPage.jsx
+++ src/pages/StatisticsPage.jsx
@@ -1,18 +1,33 @@
 import TimerBlock from 'components/TimerBlock/TimerBlock';
 import Statistics from 'components/Statistics/Statistics';
+import LineChart from 'components/Chart/Chart';
 import { Section, Container } from 'ui/BasicStyles';
 import {
 	StatisticsContainerCommon,
 	StatPagesContainer,
 } from 'ui/StatisticsPage';
+import { useSelector } from 'react-redux';
+import { getCurrentlyReading, getFinishedReading } from "../redux/library/librarySelector";
+import TrainingList from 'components/TrainingList';
 
 export const StatisticsPage = () => {
+	const currentlyReading = useSelector(getCurrentlyReading);
+	const finishedReading = useSelector(getFinishedReading);
+
+	
+
 	return (
 		<Section>
 			<Container>
 				<StatisticsContainerCommon>
-					<div>Statistic</div>
 					<TimerBlock />
+					<TrainingList
+				books={[...currentlyReading, ...finishedReading]}
+				// startDate={startDate}
+				// endDate={endDate}
+				// booksDelete={hanleDelete}
+			/>
+					<LineChart/>
 					<StatPagesContainer>
 						<Statistics />
 					</StatPagesContainer>
