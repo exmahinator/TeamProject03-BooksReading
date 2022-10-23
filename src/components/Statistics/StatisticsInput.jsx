ca82b39dd3accd2b0db00a33557f01db47f1edc0
Demchenk0 <santadyr2016@gmail.com>
2022-10-23 15:56:00 +0300

    123

--- src/components/Statistics/StatisticsInput.jsx
+++ src/components/Statistics/StatisticsInput.jsx
@@ -1,23 +1,29 @@
-import {
-	StatInput,
-	StatSubContainer,
-	StatLabel,
-	StatCommonContainer,
-} from 'ui/StatisticsPage';
+// import {
+// 	StatInput,
+// 	StatSubContainer,
+// 	StatLabel,
+// 	StatCommonContainer,
+// 	StatisticsButton,
+// } from 'ui/StatisticsPage';
 
-const StatisticsInput = () => {
-	return (
-		<StatCommonContainer>
-			<StatSubContainer>
-				<StatLabel htmlFor="data">Дата</StatLabel>
-				<StatInput id="data" type="text" />
-			</StatSubContainer>
-			<StatSubContainer>
-				<StatLabel htmlFor="pages">Кількість сторінок </StatLabel>
-				<StatInput id="pages" type="text" />
-			</StatSubContainer>
-		</StatCommonContainer>
-	);
-};
+// const StatisticsInput = () => {
+// 	return (
+// 		<StatCommonContainer>
+// 			<StatSubContainer>
+// 				<StatLabel htmlFor="data">Дата</StatLabel>
+// 				<StatInput id="data" type="text" />
+// 			</StatSubContainer>
+// 			<StatSubContainer>
+// 				<StatLabel htmlFor="pages">Кількість сторінок </StatLabel>
+// 				<StatInput id="pages" type="text" />
+// 			</StatSubContainer>
+// 			<StatisticsButton type="button" onClick={handleAddResults}>
+// 				Додати результат
+// 			</StatisticsButton>
+// 		</StatCommonContainer>
+// 	);
+// };
 
-export default StatisticsInput;
+// export default StatisticsInput;
+
+// <StatisticsButton type="button" onClick={handleAddResults}></StatisticsButton>;
