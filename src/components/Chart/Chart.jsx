d605a06eb4ecce201a88b89e084b27d089632eb9
LeonidLukin <leonidlukin90@gmail.com>
2022-10-25 02:01:58 +0300

    Implemented chart logic

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -10,7 +10,7 @@ import {
 	Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import { getStartDate, getEndDate, getPagesPerDay } from '../../redux/library/librarySelector';
+import { getStartDate, getEndDate, getPagesPerDay, getStats } from '../../redux/library/librarySelector';
 import { useSelector } from 'react-redux';
 // import { red } from '@mui/material/colors';
 
@@ -106,57 +106,11 @@ const options = {
 
 
 export default function LineChart() {
-
-	// const stats = useSelector(getStats)
+	const stats = useSelector(getStats);
 	const startDate = useSelector(getStartDate);
 	const endDate = useSelector(getEndDate);
 	const pagesPerDay = useSelector(getPagesPerDay);
-	
-	const calcDays = (startDate, endDate) => {
-		const time = Date.parse(endDate) - Date.parse(startDate);
-		let TotalDays = Math.ceil(time / (1000 * 3600 * 24));
-		const days = [];
-		
-		for (let i = 0; i < TotalDays; i++) {
-			days.push(i);
-		}
-		return days;
-	}
-	const days = calcDays(startDate, endDate);
 
-	const statsPlan = days.map((day)=>({pagesCount: pagesPerDay}))
-
-	// const dateArr = stats.map(({time})=>{
-	// 	const splitDate = time.split(' ');
-	// 	const date = Date.parse(splitDate[0]);
-	// 	return date
-	// })
-
-	// const statsFact = stats.filter(({pagesCount, time}) => {
-	// 	const pages = 0;
-
-	// 	const splitDate = time.split(' ');
-	// 	const date = Date.parse(splitDate[0]);
-
-	// 	const fact = {
-	// 		pagesCount: pages,
-	// 	}
-	// 	return fact
-	// });
-
-
-	
-	const statsFact = [
-		{ pagesCount: 5 },
-		{ pagesCount: 15 },
-		{ pagesCount: 13 },
-		// { pagesCount: 35 },
-		// { pagesCount: 12 },
-		// { pagesCount: 13 },
-	]
-
-	// let startDate = new Date('10/22/2022');
-	// let endDate = new Date('10/25/2022');
 	// const ShadowLineElement = Line.elements.Line.extend({
 	// 	draw() {
 	// 		console.log(this);
@@ -186,10 +140,46 @@ export default function LineChart() {
 	// 	datasetElementType: ShadowLineElement,
 	// });
 
-	// const days = [1, 2, 3, 4, 5, 6];
+	const generateDateArr = function(start, end) {
+		const startDate = new Date(start);
+		const endDate = new Date(end);
+		const arr = [];
+		const dt = new Date(startDate);
+
+		while (dt <= endDate) {
+			const fullDate = `${dt.getFullYear()}-${dt.getMonth() + 1}-${dt.getDate()}`;
+			arr.push({time: fullDate});
+			dt.setDate(dt.getDate() + 1);
+		}
+		return arr;
+	}
+
+	const datesArr = generateDateArr(startDate, endDate);
+	
+	const statsPlan = datesArr.map(stat => {
+		const newStat = {...stat};
+		newStat.pagesCount = pagesPerDay;
+		return newStat;
+	});
+
+	const factPlan = datesArr.map(day => {
+		const { time } = day;
+		const newStat = { ...day, pagesCount: 0 };
+
+		stats.forEach(el => {
+			const factDate = new Date(el.time);
+			const factFullDate = `${factDate.getFullYear()}-${factDate.getMonth() + 1}-${factDate.getDate()}`;
+
+			if(factFullDate === time) {
+				newStat.pagesCount += el.pagesCount;
+			}
+		});
+
+		return newStat;
+	})
 
 	const data = {
-		labels: days,
+		labels: datesArr.map(el => el.time),
 		datasets: [
 			{
 				label: 'План', // вычислить за сколько дней прочесть книгу и количество страниц startData & endDatta
@@ -210,7 +200,7 @@ export default function LineChart() {
 			},
 			{
 				label: 'Факт', // stats (key) > [data.количество страницї]
-				data: statsFact.map(stat => stat.pagesCount),
+				data: factPlan.map(stat => stat.pagesCount),
 				borderColor: '#FF6B08',
 				backgroundColor: '#FF6B08',
 				// boxShadow: '0px 2px 3px rgba(9, 30, 63, 0.25)',
