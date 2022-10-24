d6032b1c90c73005665d7fd9f1843b602a2206db
Vita Koval <mayevit@hotmail.com>
2022-10-24 22:54:56 +0300

    fixed chard

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -186,7 +186,7 @@ export default function LineChart() {
 	// 	datasetElementType: ShadowLineElement,
 	// });
 
-	const days = [1, 2, 3, 4, 5, 6];
+	// const days = [1, 2, 3, 4, 5, 6];
 
 	const data = {
 		labels: days,
