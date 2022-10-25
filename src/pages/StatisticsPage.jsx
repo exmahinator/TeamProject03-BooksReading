15002772ee56b27f5b831992c7eb5adefb1fa1fa
Sergey Tkachenko <106906276+UserTso@users.noreply.github.com>
2022-10-25 21:42:56 +0300

    style goals&statistics

--- src/pages/StatisticsPage.jsx
+++ src/pages/StatisticsPage.jsx
@@ -2,14 +2,20 @@ import TimerBlock from 'components/TimerBlock/TimerBlock';
 import Statistics from 'components/Statistics/Statistics';
 import LineChart from 'components/Chart/Chart';
 import { Section, Container } from 'ui/BasicStyles';
-import { StatisticsContainerCommon } from 'ui/StatisticsPage';
+import {
+	StatisticsContainerCommon,
+	BoxStatGoals,
+	BoxStatGoalsDesctop,
+	BoxStatDesctop,
+	BoxGoalsDesctop,
+} from 'ui/StatisticsPage';
 import { useSelector } from 'react-redux';
 import {
 	getCurrentlyReading,
 	getFinishedReading,
 } from '../redux/library/librarySelector';
 import TrainingList from 'components/TrainingList';
-import  MyGoals  from "../components/MyGoals/MyGoals";
+import MyGoals from '../components/MyGoals/MyGoals';
 
 export const StatisticsPage = () => {
 	const currentlyReading = useSelector(getCurrentlyReading);
@@ -18,18 +24,41 @@ export const StatisticsPage = () => {
 	return (
 		<Section>
 			<Container>
-				<TimerBlock />
-				<MyGoals />
-				<TrainingList
-					books={[...finishedReading, ...currentlyReading]}
-					// startDate={startDate}
-					// endDate={endDate}
-					// booksDelete={hanleDelete}
-				/>
-				<LineChart />
-				<StatisticsContainerCommon>
-					<Statistics />
-				</StatisticsContainerCommon>
+				<BoxStatGoals>
+					<TimerBlock />
+					<MyGoals />
+					<TrainingList
+						books={[...finishedReading, ...currentlyReading]}
+						// startDate={startDate}
+						// endDate={endDate}
+						// booksDelete={hanleDelete}
+					/>
+					<LineChart />
+					<StatisticsContainerCommon>
+						<Statistics />
+					</StatisticsContainerCommon>
+				</BoxStatGoals>
+
+				<BoxStatGoalsDesctop>
+
+					<BoxStatDesctop>
+						<TimerBlock />
+						<TrainingList
+							books={[...finishedReading, ...currentlyReading]}
+							// startDate={startDate}
+							// endDate={endDate}
+							// booksDelete={hanleDelete}
+						/>
+						<LineChart />
+					</BoxStatDesctop>
+
+					<BoxGoalsDesctop>
+						<MyGoals />
+						<StatisticsContainerCommon>
+							<Statistics />
+						</StatisticsContainerCommon>
+					</BoxGoalsDesctop>
+				</BoxStatGoalsDesctop>
 			</Container>
 		</Section>
 	);
--- src/pages/TrainingPage.jsx
+++ src/pages/TrainingPage.jsx
@@ -2,11 +2,14 @@ import MyTraining from 'components/MyTraining/MyTraining';
 import LineChart from '../components/Chart/Chart';
 // import { useDispatch, useSelector } from 'react-redux';
 import { Section, Container } from 'ui/BasicStyles';
-import GoalTrainingBox from '../ui/TrainingPage/GoalTrainingBox.styled';
+import {
+	GoalTrainingBox,
+	BoxPageTraining,
+	BoxPageStatistics,
+} from '../ui/TrainingPage/GoalTrainingBox.styled';
 import MyGoals from '../components/MyGoals/MyGoals';
 import { NavLink } from 'react-router-dom';
 // import { getCurrentlyReading } from '../redux/library/librarySelector';
-
 
 export const TrainingPage = () => {
 	// const dispatch = useDispatch();
@@ -19,12 +22,20 @@ export const TrainingPage = () => {
 	return (
 		<Section>
 			<Container>
-				<GoalTrainingBox>
-				<MyGoals />
-				<NavLink to="/statistics">Statistics</NavLink>
-				<MyTraining />
-				</GoalTrainingBox>
-				<LineChart />
+				<BoxPageTraining>
+					<NavLink to="/statistics">Statistics</NavLink>
+					<MyTraining />
+				</BoxPageTraining>
+
+				<BoxPageStatistics>
+					<GoalTrainingBox>
+						<MyGoals />
+						<NavLink to="/statistics">Statistics</NavLink>
+						<MyTraining />
+					</GoalTrainingBox>
+					<LineChart />
+				</BoxPageStatistics>
+
 			</Container>
 		</Section>
 	);
